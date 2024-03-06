bf91b71794e7a3b0e89220865ef542df9e9d92c6
taccarlo <tacchella.carlo@gmail.com>
2024-03-06 12:01:42 +0100

    first step

---
+++ lezione02/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ElementoElencoSpese from './components/ElementoElencoSpese';
+
+function App() {
+  return (
+    <>
+      <h1>Elenco spese:</h1>
+      <ElementoElencoSpese/>
+    </>
+  );
+}
+
+export default App;
---
+++ lezione02/src/components/ElementoElencoSpese.js
@@ -0,0 +1,14 @@
+
+import './ElementoElencoSpese.css';
+
+export default function ElementoElencoSpese(){
+    return  <div className="elemento-elenco">
+        <div>28 Marzo 2021</div>
+        <div className="element-elenco-descrizione">
+            <h2>Assicurazione</h2>
+        </div>
+        <div className="elemento-elenco-prezzo">
+            294.67
+        </div>
+    </div>
+}
