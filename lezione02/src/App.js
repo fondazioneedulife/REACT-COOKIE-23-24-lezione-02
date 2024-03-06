68deaf0588adaef15f934746b4f1e79c1fc1b50d
taccarlo <tacchella.carlo@gmail.com>
2024-03-06 12:22:36 +0100

    Update App.js

--- lezione02/src/App.js
+++ lezione02/src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 import ElementoElencoSpese from './components/ElementoElencoSpese';
 
 function App() {
+
+  const spese = [
+    {
+      titolo :"Assicurazione auto",
+      ammontare : 294.13,
+      data: new Date(2023,2,29)
+    },
+    {
+      titolo :"TV nuova",
+      ammontare : 300,
+      data: new Date(2023,3,1)
+    }
+  ]
+
   return (
     <>
       <h1>Elenco spese:</h1>
       <ElementoElencoSpese
-        data = {"1 Marzo 2023"}
-        ammontare={294.67}
-        titolo={"Assicurazione Auto"}
-      >
-      </ElementoElencoSpese>
+        data = {spese[0].data}
+        ammontare={spese[0].ammontare}
+        titolo={spese[0].titolo}
+      />
+      <ElementoElencoSpese
+        data = {spese[1].data}
+        ammontare={spese[1].ammontare}
+        titolo={spese[1].titolo}
+      />
     </>
   );
 }
